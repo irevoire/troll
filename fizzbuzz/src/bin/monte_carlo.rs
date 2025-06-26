ddd0ed962c0b544d2aedb6c7b4bb11e297ce2b6a
Tamo <tamo@meilisearch.com>
2025-06-26 19:38:16 +0200

    add the broken montecarlo fizzbuzz

---
+++ fizzbuzz/src/bin/monte_carlo.rs
@@ -0,0 +1,55 @@
+use rand::Rng;
+use std::env;
+
+fn main() {
+    // Récupère la fréquence (par défaut 10)
+    let freq: usize = env::args()
+        .nth(1)
+        .unwrap_or("100000000".to_string())
+        .parse()
+        .unwrap();
+
+    let mut current_number = 1;
+    let mut rng = rand::rng();
+
+    // Proportions issues de l'observation sur les 100 premiers nombres
+    let fizz_range =  0.0..33.0;
+    let buzz_range = 33.0..=(33.0+20.0);
+
+
+    // to get up to speed we're going to randomely set each counter
+    let mut fizz_count = ((33.0/100.0) * freq as f64) as usize;
+    let mut buzz_count = ((20.0/100.0) * freq as f64) as usize;
+    //let mut number_count = ((53.0/100.0) * freq as f64) as usize;
+    let mut number_count = freq - 1;
+
+
+    for i in 0..(100 * freq * 3) {
+        let angle = rng.random_range(0.0..100.0);
+
+        if fizz_range.contains(&angle) {
+            fizz_count += 1;
+            if fizz_count == freq {
+                print!("fizz");
+                fizz_count = 0;
+            }
+        } else if buzz_range.contains(&angle) {
+            buzz_count += 1;
+            if buzz_count == freq {
+                print!("buzz");
+                buzz_count = 0;
+            }
+        } else {
+            number_count += 1;
+            if number_count == freq {
+                print!("{}", current_number);
+                number_count = 0;
+            }
+        }
+
+        if i % freq == 0 {
+            current_number += 1;
+            println!();
+        }
+    }
+}
