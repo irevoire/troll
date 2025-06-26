8add36ffe742b36f745589ef6a3fee16cd6a9ec3
Tamo <tamo@meilisearch.com>
2025-06-26 21:36:50 +0200

    improve monte carlo

--- fizzbuzz/src/bin/monte_carlo.rs
+++ fizzbuzz/src/bin/monte_carlo.rs
@@ -8,46 +8,55 @@ fn main() {
         .parse()
         .unwrap();
 
-    let mut current_number = 1;
+    println!("1");
+    let mut current_number = 2;
     let mut rng = rand::rng();
 
-    let fizz_range =  0.0..33.0;
+    let fizz_range =  0.0..=33.0;
     let buzz_range = 33.0..=(33.0+20.0);
-
 
     // to get up to speed we're going to randomely set each counter
     let mut fizz_count = ((33.0/100.0) * freq as f64) as usize;
     let mut buzz_count = ((20.0/100.0) * freq as f64) as usize;
-    //let mut number_count = ((53.0/100.0) * freq as f64) as usize;
-    let mut number_count = freq - 1;
+    let mut number_count = ((53.0/100.0) * freq as f64) as usize;
 
+    let mut line_count = 0;
+    let mut iter = 0;
+    let mut ret = String::new();
 
-    for i in 0..(100 * freq * 3) {
+    loop {
         let angle = rng.random_range(0.0..100.0);
 
         if fizz_range.contains(&angle) {
             fizz_count += 1;
             if fizz_count == freq {
-                print!("fizz");
+                ret.push_str("fizz");
                 fizz_count = 0;
             }
         } else if buzz_range.contains(&angle) {
             buzz_count += 1;
             if buzz_count == freq {
-                print!("buzz");
+                ret.push_str("buzz");
                 buzz_count = 0;
             }
         } else {
             number_count += 1;
             if number_count == freq {
-                print!("{}", current_number);
+                ret.push_str(&current_number.to_string());
                 number_count = 0;
             }
         }
 
-        if i % freq == 0 {
+        if iter >= freq && !ret.is_empty() {
             current_number += 1;
-            println!();
+            line_count += 1;
+            if line_count == 100 {
+                break;
+            }
+            println!("{ret}");
+            ret.clear();
+            iter = 0;
         }
+        iter += 1;
     }
 }
